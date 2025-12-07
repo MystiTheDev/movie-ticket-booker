fb53de33986a2af455ec950161ffc5690670a0b2
MystiTheDev <MysticleLive@gmail.com>
2025-12-07 12:15:24 +0530

    Updated TheatreAdmin to have a new feature
    Added a new option for the admin to add new movies to the list.

--- src/reservation/client/TheatreAdmin.java
+++ src/reservation/client/TheatreAdmin.java
@@ -9,6 +9,7 @@ public class TheatreAdmin {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Theatre Admin Panel v0.1!");
         System.out.println("1. Reset Number of available seats in Theatre");
+        System.out.println("2. Add a new movie");
         System.out.print("Enter your choice: ");
         int choice = sc.nextInt();
 
@@ -16,6 +17,7 @@ public class TheatreAdmin {
         {
             case 1:
                 //Admin command to reset number of seats
+
                 System.out.println("Current amount of seats: " +server.readSeatsFile());
                 System.out.println("Enter number of seats to be reset to: ");
                 int seat = sc.nextInt();
@@ -24,7 +26,14 @@ public class TheatreAdmin {
                 System.out.println("New current amount of seats: " +server.readSeatsFile());
                 break;
 
-            
+            case 2:
+                //Admin command to add a new movie
+                
+                server.addNewMovie();
+                System.out.println("New movie list is: ");
+                server.displayMovieName();
+                break;
+                
             default:
                 System.out.println("Invalid choice!!");
         
